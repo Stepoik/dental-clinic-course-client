649fc036abbf25436279ed6aeaaab8e8e6dc9086
Stepoik <83645255+Stepoik@users.noreply.github.com>
2024-04-22 14:00:21 +0300

    изменил вьюмоедль

--- app/src/main/java/ru/mirea/dentalclinic/homescreen/HomeViewModel.kt
+++ app/src/main/java/ru/mirea/dentalclinic/homescreen/HomeViewModel.kt
@@ -2,18 +2,21 @@ package ru.mirea.dentalclinic.homescreen
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import ru.mirea.dentalclinic.domain.usecases.GetBestDoctorsUseCase
 import ru.mirea.dentalclinic.domain.usecases.GetProceduresUseCase
 import ru.mirea.dentalclinic.homescreen.formatters.DoctorFormatter
 import ru.mirea.dentalclinic.homescreen.formatters.ProcedureFormatter
 import javax.inject.Inject
-import kotlin.contracts.ExperimentalContracts
-import kotlin.contracts.InvocationKind
-import kotlin.contracts.contract
 
 class HomeViewModel @Inject constructor(
     private val getBestDoctorsUseCase: GetBestDoctorsUseCase,
@@ -22,11 +25,12 @@ class HomeViewModel @Inject constructor(
     private val procedureFormatter: ProcedureFormatter
 ) : ViewModel() {
     private val _state = MutableStateFlow(HomeScreenState())
-    val state: StateFlow<HomeScreenState> get() = _state
 
-    init {
-        update()
-    }
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val state: StateFlow<HomeScreenState>
+        get() = flow<HomeScreenState> { update() }
+            .flatMapLatest { _state }
+            .stateIn(viewModelScope, SharingStarted.Lazily, HomeScreenState())
 
     fun update() {
         if (state.value.isLoading) {
@@ -41,21 +45,25 @@ class HomeViewModel @Inject constructor(
             val procedureJob = async { getProceduresUseCase.execute() }
             val doctors = doctorsJob.await().onError { return@launch }
             val procedures = procedureJob.await().onError { return@launch }
-            _state.value = _state.value.copy(
-                isLoading = false,
-                error = null,
-                bestDoctors = doctors.map(doctorFormatter::format),
-                procedures = procedures.map(procedureFormatter::format)
-            )
+            _state.update { currentState ->
+                currentState.copy(
+                    isLoading = false,
+                    error = null,
+                    bestDoctors = doctors.map(doctorFormatter::format),
+                    procedures = procedures.map(procedureFormatter::format)
+                )
+            }
         }
     }
 
     private inline fun <R, T : R> Result<T>.onError(body: (Throwable) -> R): R {
         return getOrElse {
-            _state.value = _state.value.copy(
-                isLoading = false,
-                error = it.message
-            )
+            _state.update { currentState ->
+                currentState.copy(
+                    isLoading = false,
+                    error = it.message
+                )
+            }
             return body.invoke(it)
         }
     }
